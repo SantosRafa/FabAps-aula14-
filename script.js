da5e8a28696ae78bf98b5fb1740c624e30207439
Rafael Santos <rsantoslfap@gmail.com>
2020-03-03 08:13:35 -0300

    Aula 14

---
+++ script.js
@@ -0,0 +1,16 @@
+//while = enquanto
+
+x=0;
+
+while(x<=10){
+    document.write("<br/> O valor do X eh:" + x);
+
+    x++;
+}
+document.write("<br/><br/><br/>")
+//for = para
+
+for(a=0; a<=10;a++){
+    document.write("<br/> O valor do A eh:" + a);
+
+}
